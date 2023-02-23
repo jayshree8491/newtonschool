967dc9c62af4f1d28d53e9fd354866bdacae7e6d
jayshree8491 <pali.jayshree@gmail.com>
2023-02-23 17:54:20 +0530

    ns

---
+++ React-stateful list/index.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./Component/App.js";
+
+
+
+ReactDOM.render(<App />, document.getElementById( "root"));
